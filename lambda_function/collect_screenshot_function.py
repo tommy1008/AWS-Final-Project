f6667c0fa2fb1df427289fdf9214b1d8b16b0d0f
HIN0815 <43261589+HIN0815@users.noreply.github.com>
2018-10-07 23:29:22 +0800

    Add files via upload

--- lambda_function/collect_screenshot_function.py
+++ lambda_function/collect_screenshot_function.py
@@ -3,30 +3,50 @@ import json
 import os
 import datetime
 import  requests
+
+print('Loading function')
+# Get the service client
+s3 = boto3.client('s3')
+apigateway = boto3.client('apigateway')
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
 def lambda_handler(event,context):
-    # Get the service client
-    s3 = boto3.client('s3')
-    apigateway = boto3.client('apigateway')
-
     apiKey = apigateway.get_api_key(apiKey=event["requestContext"]["identity"]["apiKeyId"],includeValue=True)
+    
     now = datetime.datetime.now()
-    filename = now.strftime("screenshot_%M_%S.json")
     partition = now.strftime("year=%Y/month=%m/day=%d/hour=%H")
-    fields = {"acl": "public-read"}
 
-    conditions = [
-        {"acl": "public-read"},
-        ["content-length-range", 30, 1000]
-    ]
-
-    post = s3.generate_presigned_post(
-        Bucket=os.environ['StudentLabDataBucket'],
-        Key = f"screenshot_stream/{partition}/id={apiKey['name']}/{filename}",
-        Fields=fields,
-        Conditions=conditions
-
+    presigned_url = s3.generate_presigned_url(
+        ClientMethod='put_object',
+        Params={
+            'Bucket': os.environ['StudentLabDataBucket'],
+            'Key': f"screenshot_stream/{partition}/id={apiKey['name']}"
+        }
     )
+    
+    return respond(None, presigned_url)
 
-    files = {"file": "file_content"}
-    response = requests.post(post["url"], data=post["fields"], files=files)
-    return response
+#def lambda_handler(event,context):
+#    apiKey = apigateway.get_api_key(apiKey=event["requestContext"]["identity"]["apiKeyId"],includeValue=True)
+   
+#    now = datetime.datetime.now()
+#    partition = now.strftime("year=%Y/month=%m/day=%d/hour=%H")
+#    filename = now.strftime("screenshot_%M_%S.jpg")
+     
+#    img = join(body).decode('utf8')
+     
+#    s3.put_object(Bucket=os.environ['StudentLabDataBucket'],
+#            Key = f"screenshot_stream/{partition}/id={apiKey['name']}/{filename}",
+#            Body = open(img, 'wb'),
+#            ContentType="application/json"
+#          )
+
+#    return respond(None, apiKey["name"] + f" srceenshot events.")
