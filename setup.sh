5b9413eff0d066d388ca28cb8e260b07ec7488a2
HIN0815 <43261589+HIN0815@users.noreply.github.com>
2018-10-15 11:27:57 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
-sudo pip install yapf
+npm uninstall -g aws-sam-local
 sudo rm $(which sam)
+sudo yum -y update
+sudo yum -y install aws-cli
 pip install --user aws-sam-cli
+sudo -H pip install awscli --upgrade
+alias sam=~/.local/bin/sam
 sam --version
-ln -sfn $(which sam) ~/.c9/bin/sam
+ln -sfn ~/.local/bin/sam ~/.c9/bin/sam
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
