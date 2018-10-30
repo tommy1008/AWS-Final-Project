da404248ee717334d46754ae5645ef9ff2f4cf37
HIN0815 <43261589+HIN0815@users.noreply.github.com>
2018-10-30 16:35:49 +0800

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,6 +1,6 @@
 echo "Deploy $STACK_NAME stack"
 
-sourcebucket=cywong$STACK_NAME
+sourcebucket=final_project_$STACK_NAME
 aws s3 mb s3://$sourcebucket --region $REGION
 cp lambda_function/* venv/lib/python3.6/dist-packages
 rm package.yaml
@@ -9,9 +9,4 @@ sam package --template-file template.yaml --s3-bucket $sourcebucket --output-tem
 aws cloudformation deploy --stack-name $STACK_NAME --template-file package.yaml \
 --region $REGION --capabilities CAPABILITY_IAM \
 --parameter-overrides \
-    RunUnitTest="true" \
-    GitCommand="git clone -b server https://github.com/wongcyrus/ite3101_introduction_to_programming.git" \
-    SourceRespositoryName="ite3101_introduction_to_programming" \
-    EnableRealtimeAnalystics="true" \
-    CalendarUrl="https://calendar.google.com/calendar/ical/spe8ehlqjkv8hd7mdjs3d2g80c%40group.calendar.google.com/public/basic.ics" \
-    CourseKeywords="Introduction to Programing"
+    RunUnitTest="true"
